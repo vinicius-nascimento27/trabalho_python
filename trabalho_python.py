b993cd57bd9d3083735271014b45deab7cffa6ea
Vinicius <112131043+vinicius-nascimento27@users.noreply.github.com>
2025-09-11 21:51:20 -0300

    Update trabalho_python.py

--- trabalho_python.py
+++ trabalho_python.py
@@ -6,51 +6,54 @@ from pyspark.sql.functions import sum as spark_sum
 
 df = pd.read_csv("infracoes_veiculos_2024.csv", sep=None, engine="python", encoding="utf-8")
 
-#Limpeza
+#LIMPEZA DE DADOS
+
+#Remover espaços em branco
 df.columns = [c.replace("\ufeff", "") for c in df.columns]
 
-#Renomear
+#Renomear colunas
 df = df.rename(columns={
     "ANOMES": "ANO_MES",
     "DESC_TIPO": "TIPO_VEICULO",
     "AIT": "QTD_INFRACOES"
 })
 
-# converter para numérico
+# Converter para numérico
 df["ANO_MES"] = pd.to_numeric(df["ANO_MES"], errors="coerce")
 df["QTD_INFRACOES"] = pd.to_numeric(df["QTD_INFRACOES"], errors="coerce")
 
-# remover nulos
+# Remover valores nulos
 df = df.dropna(subset=["TIPO_VEICULO", "QTD_INFRACOES"])
 
-#Distribuição de Frequência
+
+#DISTRIBUIÇÃO DE FREQUÊNCIA
+
+#Criar sessão spark
 spark = SparkSession.builder.appName("Infracoes2024").getOrCreate()
 
-#criar DataFrame
+#Criar DataFrame
 spark_df = spark.createDataFrame(df[["TIPO_VEICULO", "QTD_INFRACOES"]])
 
-#Revisão extra
-spark_df = spark_df.na.drop(subset=["TIPO_VEICULO", "QTD_INFRACOES"])
-
+#Agrupar, somar e ordenar
 freq_spark = (
-    spark_df.groupBy("TIPO_VEICULO")
-            .agg(spark_sum("QTD_INFRACOES").alias("TOTAL_INFRACOES"))
-            .orderBy("TOTAL_INFRACOES", ascending=False)
+    spark_df.groupBy("TIPO_VEICULO") #agrupa todas as linhas pelo tipo de veículo
+            .agg(spark_sum("QTD_INFRACOES").alias("TOTAL_INFRACOES")) #soma todas as infrações de cada tipo
+            .orderBy("TOTAL_INFRACOES", ascending=False) #ordena do tipo que tem mais infrações para o que tem menos
 )
 
 print("Distribuição de Frequência:")
-freq_spark.show(truncate=False)
+freq_spark.show()
 
-#Regressão Linear Simples
-mensal = df.groupby("ANO_MES")["QTD_INFRACOES"].sum().reset_index().sort_values("ANO_MES")
-mensal["mes_idx"] = range(1, len(mensal) + 1)
+#REGRESSÃO LINEAR
+mensal = df.groupby("ANO_MES")["QTD_INFRACOES"].sum().reset_index().sort_values("ANO_MES") #Agrupa por mês e soma as infrações
+mensal["mes_idx"] = range(1, len(mensal) + 1) #cria uma numeração sequencial dos meses
 
-a, b = np.polyfit(mensal["mes_idx"], mensal["QTD_INFRACOES"], 1)
-mensal["pred"] = a * mensal["mes_idx"] + b
+a, b = np.polyfit(mensal["mes_idx"], mensal["QTD_INFRACOES"], 1) #Ajusta reta y = a*x + b
+mensal["pred"] = a * mensal["mes_idx"] + b #valores previstos pela reta
 
-print("Regressão Linear (tendência mensal):")
+print("Regressão Linear (tendência mensal):") # mostra os coeficientes da regressão
 print("Inclinação (a):", a)
-print("Intercepto (b):", b, "\n")
+print("Intercepto (b):", b, "\n") #ponto inicial da linha
 
 #grafico
 plt.scatter(mensal["mes_idx"], mensal["QTD_INFRACOES"], label="Observado")
